48e33b4e7662a45559f75c65f0b9cfa2d2c32e7f
Pepe Martinez <pepemart1996@gmail.com>
2020-08-05 09:16:36 -0600

    enviar bitarray

--- detection_server.py
+++ detection_server.py
@@ -1,10 +1,16 @@
 import socket
+import bitarray
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((socket.gethostname(),1234))
 s.listen(5)
+
+ba = bitarray.bitarray()
+message = input("Mensaje a enviar: ")
+ba.frombytes(message.encode('utf-8'))
 
 #listen forever
 while True:
     clientsocket, address = s.accept()
     print(f"Connection from {address} succesful!")
-    clientsocket.send(bytes('test message','utf-8'))
+    clientsocket.send(ba)
